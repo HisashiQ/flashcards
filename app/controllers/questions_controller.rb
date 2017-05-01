04d67f64106d0937063be01ee45c69ac354db9a6
HisashiQ <quinnkoike@gmail.com>
2017-05-02 00:25:54 +0100

    Reorder question list

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,7 +4,7 @@ class QuestionsController < ApplicationController
   # GET /questions
   # GET /questions.json
   def index
-    @questions = Question.all
+    @questions = Question.all.order(created_at: :desc)
   end
 
   # GET /questions/1
